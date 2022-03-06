f87ea88ee3649c7b64d6339ef2311cdba792e6be
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-03-06 10:04:16 +0530

    1359. Count All Valid Pickup and Delivery Options

---
+++ March/Day 6/Solution.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
+
+class Solution {
+    public int countOrders(int n) {
+        long res = 1, modulo = 1000000007;
+        for (int i = 1; i <= n; i++) {
+            res = res * i * (2 * i - 1) % modulo;
+        }
+        return (int) res;
+    }
+}
