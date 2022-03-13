2201f0472d4e668fe3bfb4584c45bf1ab9089d86
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-03-13 14:27:13 +0530

    20. Valid Parentheses

---
+++ March/Day 13/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    Stack<Character> stack = new Stack<>();
+
+    public boolean isValid(String s) {
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+
+            if ((c == '(') || (c == '{') || (c == '[')) {
+                stack.push(c);
+            } else {
+                if (stack.empty())
+                    return false;
+                char cc = stack.peek();
+                if ((c == ')' && cc == '(') || ((c == '}' && cc == '{')) || ((c == ']' && cc == '['))) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (stack.empty())
+            return true;
+        return false;
+    }
+}
