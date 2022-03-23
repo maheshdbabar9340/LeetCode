63eaaa9bae9f009e2dec59d8e515fedecde841e3
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-03-23 08:02:38 +0530

    991. Broken Calculator

---
+++ March/Day 23/Solution.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/broken-calculator/
+
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        if (startValue >= target)
+            return startValue - target;
+        if (target % 2 == 0) {
+            return 1 + brokenCalc(startValue, target / 2);
+        }
+        return 1 + brokenCalc(startValue, target + 1);
+    }
+}
+
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int result = 0;
+        while (target > startValue) {
+            if (target % 2 == 0) {
+                target /= 2;
+            } else {
+                target++;
+            }
+            result++;
+        }
+        return result + (startValue - target);
+    }
+}
