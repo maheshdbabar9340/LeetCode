203a568412b62d7c9275dcf4fbd00d37aa3b02b9
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-04-13 09:00:52 +0530

    59. Spiral Matrix II

--- April/Day 13/Solution.java
+++ April/Day 13/Solution.java
@@ -49,3 +49,44 @@ class Solution {
         }
     }
 }
+
+// Second Solution
+
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] ans = new int[n][n];
+        int r1 = 0, r2 = n - 1;
+        int c1 = 0, c2 = n - 1;
+        int val = 1;
+
+        while (r1 <= r2 && c1 <= c2) {
+
+            // moving from left to right
+            for (int c = c1; c <= c2; c++)
+                ans[r1][c] = val++;
+
+            // move down
+            for (int r = r1 + 1; r <= r2; r++)
+                ans[r][c2] = val++;
+
+            // move right to left
+            // move up
+
+            if (r1 < r2 && c1 < c2) {
+                // move right to left
+
+                for (int c = c2 - 1; c > c1; c--)
+                    ans[r2][c] = val++;
+
+                for (int r = r2; r > r1; r--)
+                    ans[r][c1] = val++;
+            }
+
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+    }
+}
