cc2448af9bac12cfd34f81c01ab92e329e0903e2
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-04-14 08:22:48 +0530

    700. Search in a Binary Search Tree

--- April/Day 14/Solution.java
+++ April/Day 14/Solution.java
@@ -1,5 +1,10 @@
 // https://leetcode.com/problems/search-in-a-binary-search-tree/
 
+// Recursive Approach
+
+// Time Complexity : O(N)
+// Space Complexity :O(N)
+
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if (root == null || root.val == val)
@@ -7,5 +12,23 @@ class Solution {
         if (root.val > val)
             return searchBST(root.left, val); // Value is greater than root's val then search in right side of the root
         return searchBST(root.right, val);// Value is smaller than root's val then search in left side of the root
+    }
+}
+
+// Itearative Appraoch
+
+// Time Complexity : O(N)
+// Space Complexity :O(1)
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null)
+            return null;// Base Condition(If tree is empty )
+        while (root != null && root.val != val) {// Tree is not empty and root value is not equals to val
+            root = val < root.val ? root.left : root.right;// terminatory condition if val smaller than root value then
+                                                           // search in left side else on right side
+        }
+        return root;
     }
 }
