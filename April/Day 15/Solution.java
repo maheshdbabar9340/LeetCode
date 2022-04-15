33ba17118a108222d30fb85b17dfd90f92b97d23
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-04-15 08:45:25 +0530

    669. Trim a Binary Search Tree

---
+++ April/Day 15/Solution.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/trim-a-binary-search-tree/
+
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return null;
+
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
