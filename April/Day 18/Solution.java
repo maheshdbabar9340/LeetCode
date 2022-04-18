c2596f0a7885ff5b4adb4dc376760eb374d686c1
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-04-18 09:09:18 +0530

    230. Kth Smallest Element in a BST

---
+++ April/Day 18/Solution.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+class Solution {
+    int k;
+    int result = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        inorder(root);
+        return result;
+    }
+
+    void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        if (--k == 0) {
+            result = root.val;
+            return;
+        }
+        inorder(root.right);
+    }
+}
