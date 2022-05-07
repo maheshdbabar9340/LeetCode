63b9677c7514f0c66337471fd71d9831d5f93817
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-05-07 08:41:40 +0530

    456. 132 Pattern

---
+++ May/Day 7/Solutioin.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/132-pattern/
+
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+
+        int thirdElement = Integer.MIN_VALUE;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < thirdElement)
+                return true;
+            while (!st.isEmpty() && st.peek() < nums[i])
+                thirdElement = st.pop();
+            st.push(nums[i]);
+        }
+        return false;
+    }
+}
