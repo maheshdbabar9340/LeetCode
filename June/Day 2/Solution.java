4effc1f22c7ff551f481a9d4b7180e9c271b2a1e
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-06-02 08:18:15 +0530

    867. Transpose Matrix

---
+++ June/Day 2/Solution.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/transpose-matrix/
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        int[][] transposedMatrix = new int[m][n];
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                transposedMatrix[i][j] = matrix[j][i];
+
+        return transposedMatrix;
+    }
+}
