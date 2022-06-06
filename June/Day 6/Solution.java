1ddd2577633bb7886477331839746c5865249d6d
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-06-06 09:50:50 +0530

    160. Intersection of Two Linked Lists

---
+++ June/Day 6/Solution.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode ptrA = headA, ptrB = headB;
+        while (ptrA != ptrB) { // loop until we found the first common node
+            ptrA = ptrA == null ? headB : ptrA.next; // once we're done with A, move to B
+            ptrB = ptrB == null ? headA : ptrB.next; // once we're done with B, move to A
+        }
+        return ptrA;
+    }
+}
