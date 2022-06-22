e76ddd5bdc22293312acd37b1ce31a28b67bd180
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-06-22 08:44:40 +0530

    215. Kth Largest Element in an Array

---
+++ June/Day 22/Solution.java
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/ 
+
+// First Approach - Using Sorting
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
+
+// Second Approach
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        if (left == right)
+            return nums[left];
+
+        int pIndex = new Random().nextInt(right - left + 1) + left;
+        pIndex = partition(nums, left, right, pIndex);
+
+        if (pIndex == k)
+            return nums[k];
+        else if (pIndex < k)
+            return quickSelect(nums, pIndex + 1, right, k);
+        return quickSelect(nums, left, pIndex - 1, k);
+    }
+
+    private int partition(int[] nums, int left, int right, int pIndex) {
+        int pivot = nums[pIndex];
+        swap(nums, pIndex, right);
+        pIndex = left;
+
+        for (int i = left; i <= right; i++)
+            if (nums[i] <= pivot)
+                swap(nums, i, pIndex++);
+
+        return pIndex - 1;
+    }
+
+    private void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+}
