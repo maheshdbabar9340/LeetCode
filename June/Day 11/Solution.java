5ec4bcb7923a119fb37d34f522362420a7490a23
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-06-24 08:21:31 +0530

    Update Solution.java

--- June/Day 11/Solution.java
+++ June/Day 11/Solution.java
@@ -14,7 +14,7 @@ class Solution {
             if (currSum == sum - x)
                 maxLength = Math.max(maxLength, r - l + 1);
         }
-
+        
         return maxLength == -1 ? -1 : nums.length - maxLength;
     }
 }
