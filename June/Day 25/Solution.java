71bfed21281ad3421b8ed79c76f06fa4540e71f4
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-06-25 10:18:50 +0530

    665. Non-decreasing Array

---
+++ June/Day 25/Solution.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/non-decreasing-array/
+
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        for (int i = 1, modified = 0, prev = nums[0]; i < nums.length; i++) {
+            if (nums[i] < prev) {
+                if (modified++ == 1)
+                    return false;
+                if (i >= 2 && nums[i - 2] > nums[i])
+                    continue;
+            }
+            prev = nums[i];
+        }
+        return true;
+    }
+}
