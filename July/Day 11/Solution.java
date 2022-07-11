819eee2f94c8e36e6dec8803ff6a8a86f7157eac
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-07-11 10:37:16 +0530

    199. Binary Tree Right Side View

--- July/Day 11/Solution.java
+++ July/Day 11/Solution.java
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/binary-tree-right-side-view/
 
+// Solution 1:
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list = new ArrayList<Integer>();
@@ -15,5 +17,35 @@ class Solution {
             helper(node.right, list, depth + 1);
             helper(node.left, list, depth + 1);
         }
+    }
+}
+
+// Solution 2:
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        if (root == null)
+            return list;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size(); // store the number of nodes of current layer
+            TreeNode node = null;
+            while (size > 0) {
+                node = queue.poll();
+                if (node.left != null) // add next layer's node
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+                size--;
+            }
+            list.add(node.val); // add the val of last node
+        }
+
+        return list;
     }
 }
