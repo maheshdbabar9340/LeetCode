393f668cfaa09783f778300754ac62ca1880a78f
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-07-13 10:39:46 +0530

    102. Binary Tree Level Order Traversal

---
+++ July/Day 13/Solution.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> currLevel = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode curr = q.poll();
+                currLevel.add(curr.val);
+                if (curr.left != null)
+                    q.offer(curr.left);
+                if (curr.right != null)
+                    q.offer(curr.right);
+
+            }
+            result.add(currLevel);
+        }
+        return result;
+    }
+}
