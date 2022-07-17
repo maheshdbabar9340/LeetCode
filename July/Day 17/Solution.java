95a30f5850d56d6689becfe1aadecd1d00dd900a
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-07-17 09:22:30 +0530

    629. K Inverse Pairs Array

--- July/Day 17/Solution.java
+++ July/Day 17/Solution.java
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/k-inverse-pairs-array/
 
+// Solution 1:
+
 class Solution {
     public int kInversePairs(int n, int k) {
         int MOD = 1000000007;
@@ -18,5 +20,26 @@ class Solution {
         }
 
         return opt[k][n - 1];
+    }
+}
+
+// Solution 2:
+
+public class Solution {
+    Integer[][] memo = new Integer[1001][1001];
+    int M = 1000000007;
+    public int kInversePairs(int n, int k) {
+        return ((inv(n, k) + M - (k > 0 ? inv(n, k - 1) : 0)) % M);
+    }
+    public int inv(int n, int k) {
+        if (n == 0)
+            return 0;
+        if (k == 0)
+            return 1;
+        if (memo[n][k] != null)
+            return memo[n][k];
+        int val = (inv(n - 1, k) + M - ((k - n) >= 0 ? inv(n - 1, k - n) : 0)) % M;
+        memo[n][k] = (inv(n, k - 1) + val) % M;
+        return memo[n][k];
     }
 }
