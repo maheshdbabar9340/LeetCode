8d3c5b161496a9584c678bfa99f74fdbbbde7963
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-07-27 12:43:50 +0530

    114. Flatten Binary Tree to Linked List

---
+++ July/Day 27/Solution.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        while (root != null) {
+            if (root.left != null) {
+                TreeNode Left = root.left;
+                TreeNode curr = Left;
+                while (curr.right != null)
+                    curr = curr.right;
+                curr.right = root.right; // Morris Traversal main step
+                root.left = null;
+                root.right = Left;
+            }
+            root = root.right;
+        }
+    }
+}
