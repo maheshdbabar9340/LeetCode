0177347320462caa3ff64fbd2d4a475359c1152f
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-07-28 21:08:18 +0530

    242. Valid Anagram

--- July/Day 28/Solution.java
+++ July/Day 28/Solution.java
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/valid-anagram/
 
+// Approach 1: Using Sorting
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length())
@@ -16,5 +18,27 @@ class Solution {
                 return false;
         }
         return true;
+    }
+}
+
+// Approach 2: Using HashMap
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (map.containsKey(t.charAt(i)) == false)
+                return false;
+            if (map.get(t.charAt(i)) > 1)
+                map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+            else
+                map.remove(t.charAt(i));
+        }
+        return map.size() == 0;
     }
 }
