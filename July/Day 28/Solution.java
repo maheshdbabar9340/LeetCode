8cb7621167740a17a5aa2cd3353a96745b1a8458
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-07-28 21:07:12 +0530

    242. Valid Anagram

---
+++ July/Day 28/Solution.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        char cs[] = s.toCharArray();
+        char ct[] = t.toCharArray();
+
+        Arrays.sort(cs);
+        Arrays.sort(ct);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (cs[i] != ct[i])
+                return false;
+        }
+        return true;
+    }
+}
