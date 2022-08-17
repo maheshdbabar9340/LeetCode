4a47e9a258289201829fb2885a442b60952b412b
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-08-17 09:02:58 +0530

    Day 1

--- Programming Skills/Programming Skills 2/Day 1/28. Implement strStr()/Solution.java
+++ Programming Skills/Programming Skills 2/Day 1/28. Implement strStr()/Solution.java
@@ -1,7 +1,25 @@
 // https://leetcode.com/problems/implement-strstr/
 
+// First Solution : Using indexOf() method
 class Solution {
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
+    }
+}
+
+// Second Solution: Using simple tracing
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty())
+            return 0;
+        int i = 0;
+        int j = needle.length();
+        while ((i + j) <= haystack.length()) {
+            if (haystack.substring(i, i + j).equals(needle))
+                return i;
+            i++;
+        }
+        return -1;
     }
 }
