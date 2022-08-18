d49827e93d331f47d0d3811d9addeaadb98a5705
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-08-18 08:38:33 +0530

    Description added for System.arraycopy() method

--- Data Structure/Data Structure 1/Day 2/88. Merge Sorted Array/Solution.java
+++ Data Structure/Data Structure 1/Day 2/88. Merge Sorted Array/Solution.java
@@ -2,6 +2,13 @@
 
 // First Solution : Using System.arraycopy() method
 
+// public static void arraycopy(Object source_arr, int sourcePos, Object dest_arr, int destPos, int len)
+// source_arr : array to be copied from
+// sourcePos : starting position in source array from where to copy
+// dest_arr : array to be copied in
+// destPos : starting position in destination array, where to copy in
+// len : total no. of components to be copied.
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int len1 = m - 1;
