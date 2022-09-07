1339b0e460773e14506a1200a05c14c0096ae464
Maheshdbababar <53929423+Maheshdbababar@users.noreply.github.com>
2022-09-07 22:34:18 +0530

    606. Construct String from Binary Tree

---
+++ 2022-Daily_LeetCode_Problems/September/Day 7/Solution.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/construct-string-from-binary-tree/
+
+class Solution {
+    StringBuilder sb;
+    private void doEv(TreeNode root){
+        if(root == null)return;
+        if(root.left == null && root.right==null){
+            sb.append(root.val);
+            return;
+        }
+        sb.append(root.val);
+        sb.append('(');
+        doEv(root.left);
+        sb.append(')');
+        if(root.right!=null){
+        sb.append('(');
+        doEv(root.right);
+        sb.append(')');
+        }
+        return;
+    }
+    public String tree2str(TreeNode t) {
+        sb = new StringBuilder();
+        doEv(t);
+        return sb.toString();
+    }
+}
