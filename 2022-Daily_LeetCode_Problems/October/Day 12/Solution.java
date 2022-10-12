bf45f6ae0eeb869097b3aa1c07fa48a02c8d1b04
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2022-10-12 08:51:27 +0530

    976. Largest Perimeter Triangle

---
+++ 2022-Daily_LeetCode_Problems/October/Day 12/Solution.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int max=0;
+        for(int i=n-1;i>1;i--){
+            if(nums[i]+nums[i-1]>nums[i-2] && nums[i-1]+nums[i-2]>nums[i] && nums[i]+nums[i-2]>nums[i-1])
+               max = (int)Math.max(max,nums[i]+nums[i-1]+nums[i-2]);
+        }
+        return max;
+    }
+}
