faeb5c8527fb54c416c0a03f603466358724ddab
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2023-02-13 10:06:33 +0530

    LeetCode 1523

--- Daily_LeetCode_Problems/Year_2023/February/Day 13/Solution.java
+++ Daily_LeetCode_Problems/Year_2023/February/Day 13/Solution.java
@@ -9,3 +9,19 @@ class Solution {
         return count;
     }
 }
+
+
+
+class Solution {
+    public int countOdds(int low, int high) {
+        int total = high - low + 1;
+        if(total % 2 == 0)
+            return total / 2;
+        else {
+            if(low % 2 != 0)
+                return total/2 + 1;
+            else 
+                return total/2;
+        }
+    }
+}
