1841ad46bf98ae129a3c80e6b7fcaa3adb535b97
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2023-04-04 15:28:33 +0530

    Leetcode 2405

---
+++ Daily_LeetCode_Problems/Year_2023/April/Day 3/Solution.java
@@ -0,0 +1,16 @@
+// Leetcode 2405
+
+class Solution {
+    public int partitionString(String s) {
+        int xr=0;
+        int ans=1;
+        for(char c : s.toCharArray()){
+            if((xr & (1<<c))!=0){
+                xr=0;
+                ans++;
+            }
+            xr^=(1<<c);
+        }
+        return ans;
+    }
+}
